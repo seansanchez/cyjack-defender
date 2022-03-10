e625f2cca15164a30e05449f269801631a39c87f
Sean Sanchez <atilano.e.sanchez@outlook.com>
2022-03-09 21:58:05 -0800

    Initial Commit (#1)
    * commit current
    
    * updates
    
    * updates

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cyjack-defender
+
+### Required Hardware
+* Raspberry Pi 3B+
+
+### Dependencies
+* Visual Studio 2019 / 2022
+  * Workload: Universal Windows Platform development
+* [Windows 10 IoT Core Dashboard](https://docs.microsoft.com/en-us/windows/iot-core/connect-your-device/iotdashboard)
+
+### Setup
+1. Using the Windows 10 IoT Dashboard, create a new device using "Raspberry Pi 2 & 3" as the device type and "Windows 10 IoT Core (17763)" as the OS Build.
+2. a
+3. When debugging on VS, attach debugger to the IoT device.
+
+### Endpoints
+* API: http://{ipAddress}:5000/api
+* UI: http://{ipAddress}:5000/ui
---
+++ src/RaspberryPi.Web/Controllers/ParameterController.cs
@@ -0,0 +1,19 @@
+﻿using RaspberryPi.Api.Models;
+using Restup.Webserver.Attributes;
+using Restup.Webserver.Models.Contracts;
+using Restup.Webserver.Models.Schemas;
+
+namespace RaspberryPi.Api.Controllers
+{
+    [RestController(InstanceCreationType.Singleton)]
+    public class ParameterController
+    {
+        [UriFormat("/simpleparameter/{id}/property/{propName}")]
+        public IGetResponse GetWithSimpleParameters(int id, string propName)
+        {
+            return new GetResponse(
+                GetResponse.ResponseStatus.OK,
+                new DataReceived() { ID = id, PropName = propName });
+        }
+    }
+}
