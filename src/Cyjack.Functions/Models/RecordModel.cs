88cad6e1020be44e22df11dac2f914c952759a28
Sean Sanchez <atilano.e.sanchez@outlook.com>
2022-03-23 12:23:39 -0700

    updates

---
+++ src/Cyjack.Functions/Models/RecordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cyjack.Functions.Models
+{
+    public class RecordModel
+    {
+        public string Description { get; set; }
+
+        public string ExtendedProperties { get; set; }
+    }
+}
