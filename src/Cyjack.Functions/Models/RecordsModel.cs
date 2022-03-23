b849cb77b5b57ae12a40dece2d0b7bf0e44a07d4
Sean Sanchez <atilano.e.sanchez@outlook.com>
2022-03-23 12:27:20 -0700

    Use Event Hub Trigger to Send Device Messages (#25)
    * update ui endpoints
    
    * updates

---
+++ src/Cyjack.Functions/Models/RecordsModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Cyjack.Functions.Models
+{
+    public class RecordsModel
+    {
+        [JsonProperty("records")]
+        public IEnumerable<RecordModel> Records { get; set; }
+    }
+}
