05793421239d72e3bd71b348d44f52e5b3756193
Sean Sanchez <atilano.e.sanchez@outlook.com>
2022-03-22 21:37:02 -0700

    updates

--- src/Cyjack/Constants.cs
+++ src/Cyjack/Constants.cs
@@ -1,10 +1,12 @@
 ﻿namespace Cyjack
 {
-    public static class CyjackConstants
+    public static class Constants
     {
         public static class EnvironmentSettings
         {
             public const string Environment = "ASPNETCORE_ENVIRONMENT";
+
+            public const string KeyVaultUri = "KeyVaultUri";
         }
     }
 }
